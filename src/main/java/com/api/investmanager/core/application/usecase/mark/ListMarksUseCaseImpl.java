a85cb3cf0af3daa3b945b617b4bdb35f37dffb3f
Willian17 <willianfernandes113@gmail.com>
2024-11-16 18:04:02 -0400

    feature: limpando useCases de frameworks e libs

--- src/main/java/com/api/investmanager/core/application/usecase/mark/ListMarksUseCaseImpl.java
+++ src/main/java/com/api/investmanager/core/application/usecase/mark/ListMarksUseCaseImpl.java
@@ -3,11 +3,9 @@ package com.api.investmanager.core.application.usecase.mark;
 import com.api.investmanager.core.application.port.input.mark.ListMarksUseCase;
 import com.api.investmanager.core.application.port.output.mark.ListMarksPort;
 import com.api.investmanager.core.domain.model.Mark;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public class ListMarksUseCaseImpl implements ListMarksUseCase {
     private final ListMarksPort listMarksPort;
 
